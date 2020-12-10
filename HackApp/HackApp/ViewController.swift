dcde021f65d7bea5780701beef63021a3498ff17
jhiat00 <jessehiatt4@gmail.com>
2020-12-10 14:26:16 -0600

    Created project

---
+++ HackApp/HackApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackApp
+//
+//  Created by Jesse Hiatt on 12/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
